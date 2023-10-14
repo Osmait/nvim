4d841aa176a6f5ec67b5c476979d09b1d007e13a
Saul Burgos <saulburgos6@gmail.com>
2023-10-14 19:22:09 -0400

    add lazy extras

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,20 @@
 return {
   -- add onedark
-  { "olimorris/onedarkpro.nvim" , opts= { transparency =true} },
+  { "olimorris/onedarkpro.nvim", opts = { transparency = true } },
+  --
+  -- -- Configure LazyVim to load onedark
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "onedark",
+  --   },
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
 
-  -- Configure LazyVim to load onedark
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_vivid"
-      
+      colorscheme = "onedark_vivid",
     },
-  }
-
+  },
 }
