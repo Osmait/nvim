e541ff6f9dddc11fc76a4d7fa2388f801a2d6704
osmait <saulburgos6@gmail.com>
2023-10-18 22:57:57 -0400

    add dbUI

---
+++ lua/plugins/DBUI.lua
@@ -0,0 +1,14 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  },
+  keys = { { "<leader><leader>n", ":tab DBUI<cr>", desc = "Open Database client" } },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+}
