d98333f514eb5ae364cd7a15df81b6f041924742
osmait <saulburgos6@gmail.com>
2023-10-19 13:02:26 -0400

    add keymapper for gopher

--- lua/plugins/gopher.lua
+++ lua/plugins/gopher.lua
@@ -4,4 +4,10 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  keys = {
+    { "<leader>gsj", "<cmd> GoTagAdd json <CR>", "Add json struct tags" },
+    { "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", "Add yaml struct tags" },
+    { "<leader>gat", "<cmd>GoTestsAll<CR>", "Add Test for All File" },
+    { "<leader>gie", "<cmd>GoIfErr<CR>", "Add if Err" },
+  },
 }
