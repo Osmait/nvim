1894a0a55b3d4599ea5e0a249c97adecc50e0641
osmait <saulburgos6@gmail.com>
2023-11-19 20:35:44 -0400

    add jest support

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,12 @@ return {
   --   },
   -- },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 
   {
     "LazyVim/LazyVim",
