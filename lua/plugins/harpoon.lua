3a53ddbbd95dc6bf5bb7835fb42a4b9eb765a4a8
osmait <saulburgos6@gmail.com>
2023-12-04 18:25:57 -0400

    update harpoon key

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,22 +1,15 @@
 return {
-    'ThePrimeagen/harpoon',
-   keys= {
-       { "<C-t>",
-       function() require("harpoon.ui").toggle_quick_menu()end,
-    },
-    {
-        "<C-a>",
-       function() require("harpoon.mark").add_file()end,
-    },
-
-    {
-        "<C-h>",
-       function() require("harpoon.ui").nav_next()end,
-    },
-
-    {
-        "<C-l>",
-       function() require("harpoon.ui").nav_prev()end,
-    }
+  "ThePrimeagen/harpoon",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = true,
+  keys = {
+    { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+    { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+    { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
+    { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+  },
 }
-}
+
